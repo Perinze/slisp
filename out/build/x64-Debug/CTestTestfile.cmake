4e65224afa5adc06e6c668909b42faa2ae1b1e9d
perinze <perinzefaper@foxmail.com>
2024-02-24 12:11:26 +0800

    init

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/3574/Proj2
+# Build directory: C:/3574/Proj2/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittests "unittests")
+set_tests_properties(unittests PROPERTIES  _BACKTRACE_TRIPLES "C:/3574/Proj2/CMakeLists.txt;51;add_test;C:/3574/Proj2/CMakeLists.txt;0;")
---
+++ out/build/x64-Debug/test_config.hpp
@@ -0,0 +1,10 @@
+#ifndef TEST_CONFIG_HPP
+#define TEST_CONFIG_HPP
+
+#include <string>
+
+const std::string TEST_FILE_DIR = "C:/3574/Proj2/tests";
+
+#endif
+
+
