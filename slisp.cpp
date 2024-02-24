e762f4c0e5b2085f254d4bc1f076bac8f9ddf5c5
perinze <perinzefaper@foxmail.com>
2024-02-24 17:16:03 +0800

    slisp

--- slisp.cpp
+++ slisp.cpp
@@ -3,25 +3,31 @@
 #include <sstream>
 #include "tokenize.hpp"
 #include "expression.hpp"
+#include "interpreter.hpp"
 
 int main(int argc, char **argv)
 {
-  TokenSequenceType tokens = tokenize(std::cin);
+  //TokenSequenceType tokens = tokenize(std::cin);
 
-  for (auto s : tokens) {
-    std::cout << "'" << s << "' ";
-  }
-  std::cout << std::endl;
+  //for (auto s : tokens) {
+  //  std::cout << "'" << s << "' ";
+  //}
+  //std::cout << std::endl;
 
-  // test token to atom
-  Atom a;
-  for (auto s : tokens) {
-    if (token_to_atom(s, a)) {
-      std::cout << a << " ";
-    } else {
-      std::cout << "invalid ";
-    }
-  }
-  std::cout << std::endl;
+  //// test token to atom
+  //Atom a;
+  //for (auto s : tokens) {
+  //  if (token_to_atom(s, a)) {
+  //    std::cout << a << " ";
+  //  } else {
+  //    std::cout << "invalid ";
+  //  }
+  //}
+  //std::cout << std::endl;
+
+  // test intepreter
+  Interpreter interpreter;
+  interpreter.parse(std::cin);
+
   return EXIT_SUCCESS;
 }
