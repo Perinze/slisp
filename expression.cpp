727c06571d1d131ce9e11f2fa0263f01c8ad60db
perinze <perinzefaper@foxmail.com>
2024-02-24 15:09:15 +0800

    expression

--- expression.cpp
+++ expression.cpp
@@ -3,35 +3,96 @@
 #include <cmath>
 #include <limits>
 #include <cctype>
+#include <exception>
 
 // system includes
 #include <sstream>
 
 Expression::Expression(bool tf){
-  // TODO: implement this function
+  head.type = BooleanType;
+  head.value = Value{.bool_value=tf};
 }
 
 Expression::Expression(double num){
-  // TODO: implement this function
+  head.type = NumberType;
+  head.value = Value{.num_value=num};
 }
 
 Expression::Expression(const std::string & sym){
-  // TODO: implement this function
+  head.type = SymbolType;
+  head.value = Value{.sym_value=sym};
 }
 
 bool Expression::operator==(const Expression & exp) const noexcept{
-  // TODO: implement this function
-  return false;
+  if (head.type != exp.head.type) return false;
+  switch (head.type)
+  {
+  case BooleanType:
+    if (head.value.bool_value != exp.head.value.bool_value)
+      return false;
+    break;
+
+  case NumberType:
+    if (head.value.num_value != exp.head.value.num_value)
+      return false;
+    break;
+
+  case SymbolType:
+    if (head.value.sym_value != exp.head.value.sym_value)
+      return false;
+    break;
+  
+  default:
+    break;
+  }
+  if (tail.size() != exp.tail.size()) return false;
+  return true;
 }
 
 std::ostream & operator<<(std::ostream & out, const Expression & exp){
-  // TODO: implement this function
+  out << "(";
+  switch (exp.head.type)
+  {
+  case BooleanType:
+    out << (exp.head.value.bool_value ? "True" : "False");
+    break;
+  case NumberType:
+    out << exp.head.value.num_value;
+    break;
+  case SymbolType:
+    out << exp.head.value.sym_value;
+    break;
+  default:
+    break;
+  }
+  for (auto e : exp.tail) {
+    out << e;
+  }
+  out << ")";
   return out;
 }
 
 bool token_to_atom(const std::string & token, Atom & atom){
-  // TODO: implement this function
   // return true if it a token is valid. otherwise, return false.
+  if (token == "(" || token == ")") {
+    return false;
+  } else if (token == "True") {
+    atom.type = BooleanType;
+    atom.value.bool_value = true;
+  } else if (token == "False") {
+    atom.type = BooleanType;
+    atom.value.bool_value = false;
+  } else { // Number or Symbol
+    try {
+      atom.type = NumberType;
+      atom.value.num_value = std::stod(token);
+    } catch (const std::invalid_argument&) {
+      atom.type = SymbolType;
+      atom.value.sym_value = token;
+    } catch (const std::out_of_range&) {
+      return false;
+    }
+  }
   return true;
 }
 
--- slisp.cpp
+++ slisp.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include "tokenize.hpp"
+#include "expression.hpp"
 
 int main(int argc, char **argv)
 {
@@ -16,6 +17,17 @@ int main(int argc, char **argv)
 
   for (auto s : tokens) {
     std::cout << "'" << s << "' ";
+  }
+  std::cout << std::endl;
+
+  // test token to atom
+  Atom a;
+  for (auto s : tokens) {
+    if (token_to_atom(s, a)) {
+      std::cout << a << " ";
+    } else {
+      std::cout << "invalid ";
+    }
   }
   std::cout << std::endl;
   return EXIT_SUCCESS;
