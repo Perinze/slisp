16881e3dd44d1a18e1883ea587a2727f8bf6d968
perinze <perinzefaper@foxmail.com>
2024-02-24 19:07:11 +0800

    env

--- environment.cpp
+++ environment.cpp
@@ -5,6 +5,37 @@
 
 #include "interpreter_semantic_error.hpp"
 
+EnvResult proc_add = {
+  .type=ProcedureType,
+  .proc=[](const std::vector<Atom>&args) -> Expression {
+    Number sum = 0.;
+    for (auto a : args) {
+      // TODO: check for non-number
+      sum += a.value.num_value;
+    }
+    return sum;
+  }
+};
+
+
 Environment::Environment(){
-  // TODO: implement your code ...
+  // add default env
+  envmap["+"] = proc_add;
+}
+
+bool Environment::lookup(Symbol sym, EnvResult &res) {
+  auto it = envmap.find(sym);
+  if (it == envmap.end()) {
+    return false;
+  }
+  res = it->second;
+  return true;
+}
+
+bool Environment::define(Symbol sym, Expression exp) {
+  if (envmap.find(sym) != envmap.end()) {
+    return false;
+  }
+  envmap[sym] = {.type=ExpressionType, .exp=exp};
+  return true;
 }
